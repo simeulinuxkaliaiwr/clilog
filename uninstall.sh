ec34b0ac999d334296ad5caf37fb59d19cbc97a0
Gui <leonsombrio244@gmail.com>
2025-09-29 23:10:17 -0300

    Added a uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Starting Clilog Uninstallation... (Requires sudo!)"
+read -rp "Do you REALLY want to delete clilog from your OS? (s/n): " confirm
+if [[ -z "$confirm" ]] || [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+	echo "Ok! Exiting..."
+	exit 0
+fi
+
+sudo rm -rf "/usr/local/lib/clilog"
+sudo rm -f "/usr/local/bin/clilog"
+printf "\e[32mDesinstallation Completed Succesfully!\e[0m"
