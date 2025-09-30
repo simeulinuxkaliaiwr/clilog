cc932bb74a79b670502435deb4b755f4b8a10e59
Gui <leonsombrio244@gmail.com>
2025-09-30 17:45:39 -0300

    Updated the uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -11,4 +11,10 @@ fi
 
 sudo rm -rf "/usr/local/lib/clilog"
 sudo rm -f "/usr/local/bin/clilog"
-printf "\e[32mDesinstallation Completed Succesfully!\e[0m"
+sudo rm -f "/usr/local/share/man/man1/clilog.1"
+EXIT_STATUS=$?
+if [[ "$EXIT_STATUS" -eq 0 ]]; then
+	printf "\e[32mDesinstallation Completed Succesfully!\e[0m"
+else
+	printf "\033[31mError: Desinstallation failed, EXIT STATUS: $EXIT_STATUS\033[0m"
+fi
